ef5fb0b848ae3e25c46ca8d0b6bb86cf59c5c96b
Heberth López <heberth.lopez.19@gmail.com>
2024-10-12 01:26:43 -0500

    feat: :tada: Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import http from "node:http";
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const requestListener = (req: http.IncomingMessage, res: http.ServerResponse) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.end(
+    `<h1>Explora Peru API 🚀</h1>
+    <p>This is the backend of the Explora Peru project.</p>`
+  );
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
