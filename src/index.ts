793500e2f1086789b198d43f19c237452f1c9d01
Heberth López <heberth.lopez.19@gmail.com>
2024-10-12 23:02:31 -0500

    chore: :test_tube: Testing biome with lint-staged

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-import http from "node:http";
+import http from 'node:http';
 
-const hostname = "127.0.0.1";
+const hostname = '127.0.0.1';
 const port = 3000;
 
-const requestListener = (_req: http.IncomingMessage, res: http.ServerResponse) => {
+const requestListener = (
+  _req: http.IncomingMessage,
+  res: http.ServerResponse
+) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.end(
     `<h1>Explora Peru API 🚀</h1>
     <p>This is the backend of the Explora Peru project.</p>`
