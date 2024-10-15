3e17f3fa1f87cc82341086bbc805069215285efe
Heberth López <heberth.lopez.19@gmail.com>
2024-10-15 00:13:02 -0500

    feat: :sparkles: Created User services and added createTourist function

---
+++ src/models/User.ts
@@ -0,0 +1,5 @@
+export interface UserEntry {
+  email: string;
+  password: string;
+  phoneNumber: string;
+}
---
+++ src/services/user.services.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import type { UserEntry } from '../models/User';
+
+const prisma = new PrismaClient();
+
+export const createUser = async (data: UserEntry) => {
+  try {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        username: crypto.randomUUID(),
+        email: data.email,
+        password: hashedPassword,
+        phoneNumber: data.phoneNumber,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    });
+
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error while creating the user');
+  }
+};
