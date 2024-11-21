446b5d374fa326ad375fcd092dce5a4b53909ac9
Heberth López <heberth.lopez.19@gmail.com>
2024-11-21 18:38:56 -0500

    fix: :ambulance: Updated getTourSessions function to include range of days filter

--- src/services/session.services.ts
+++ src/services/session.services.ts
@@ -17,21 +17,69 @@ export const createTourSession = async (data: {
     });
   } catch (error) {
     console.error(error);
-    throw new Error('Error creating the session');
+    throw new Error('Error creating the tour session');
   }
 };
 
-export const getTourSessions = async (
+export const getTourSessionsbyDate = async (
   tourId?: number,
-  date?: Date | string
-) => {
-  if (!tourId && !date) {
-    return await prisma.session.findMany();
-  }
+  date?: Date | string,
+  rangeOfDays?: number
+): Promise<Session[]> => {
+  try {
+    const startDate = new Date(date as string);
+    startDate.setHours(0, 0, 0, 0);
 
-  return await prisma.session.findMany({
-    where: {
-      AND: [{ tourId }, { sessionDate: date }]
+    const endDate = new Date(date as string);
+    endDate.setHours(23, 59, 59, 999);
+
+    if (!tourId && !date) {
+      return await prisma.session.findMany({
+        where: {
+          sessionDate: {
+            gte: startDate,
+            lte: endDate
+          }
+        }
+      });
     }
-  });
+
+    if (rangeOfDays) endDate.setDate(endDate.getDate() + rangeOfDays);
+
+    return await prisma.session.findMany({
+      where: {
+        AND: [
+          { tourId },
+          {
+            sessionDate: {
+              gte: startDate,
+              lte: endDate
+            }
+          }
+        ]
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error getting the tour sessions');
+  }
+};
+
+export const findTourSession = async (
+  tourId: number,
+  sessionDate: Date | string
+): Promise<Session | null> => {
+  try {
+    return await prisma.session.findUnique({
+      where: {
+        tourId_sessionDate: {
+          tourId,
+          sessionDate
+        }
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error finding the tour session');
+  }
 };
